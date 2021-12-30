3de5a4ad703a1b5f02faaa2fc1189505fcd3ad7b
gianlucagiudice <gianlu.giudice@gmail.com>
2021-12-31 00:28:49 +0100

    Resnet50 training test

---
+++ src/resnet.py
@@ -0,0 +1,27 @@
+from src.models.ModelFromScratch import ModelFromScratch
+from src.DataManager import DataManager
+from config import IMBD_CROPPED_METADATA_FILENAME, IMDB_CROPPED_PATH
+from src.models.Model import IMAGE_INPUT_SIZE
+
+N_SAMPLE = .01
+
+# Read the data
+data_manager = DataManager(IMDB_CROPPED_PATH, IMBD_CROPPED_METADATA_FILENAME, IMAGE_INPUT_SIZE, n_subset=N_SAMPLE)
+data = data_manager.get_dataset()
+
+# Split into train, validation, test
+train, validation, test = data_manager.split_dataset(data)
+
+# Read images
+print('Read training images ...')
+X_train, y_train = data_manager.get_X(train), data_manager.get_y(train)
+print('Read validation images ...')
+X_val, y_val = data_manager.get_X(validation), data_manager.get_y(validation)
+print('Read test images ...')
+X_test, y_test = data_manager.get_X(test), data_manager.get_y(test)
+
+model_from_scratch = ModelFromScratch()
+model_from_scratch.train(X_train, y_train, X_val, y_val, X_test, y_test)
+
+
+
