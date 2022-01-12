51ea56bf2633dc8a5703b0c83792fb49ea7d4b13
lucreziatonelli <lucreziatonelli17@gmail.com>
2022-01-12 16:27:22 +0100

    Regression and classification (just the notebooks, I have to include them in the classes)

--- src/models/HandcraftedModel.py
+++ src/models/HandcraftedModel.py
@@ -55,9 +55,10 @@ class HandcraftedModel(MyModel):
         df = pd.DataFrame()
 
         print('Extracting dataset features ...')
-        with tqdm(total=X.shape[0]) as pbar:
+        with tqdm(total=len(X)) as pbar:
             for x in X:
                 x = self.equalize_histogram(x)
+                x = cv2.bilateralFilter(x, 15, 75, 75)
                 features = self.extract_features(x)
                 df = df.append(features, ignore_index=True)
                 pbar.update(1)
@@ -110,17 +111,6 @@ class HandcraftedModel(MyModel):
         for s in img_sift:
             df[i] = s
             i = i + 1
-
-        #        for i in range(len(img_color_hist)):  # 12 arrays of 256 numbers
-        #            col_name = 'color_hist_' + str(i)
-        #            df[col_name] = img_color_hist[i]
-        #        for i in range(len(img_lbp)):  # 4 arrays of 26 numbers
-        #            col_name = 'lbp_' + str(i)
-        #            df[col_name] = img_lbp[i]
-
-        #        for i in range(len(img_sift)):  # n arrays of 128 numbers
-        #            col_name = 'sift_' + str(i)
-        #            df[col_name] = img_sift[i]
 
         features = pd.DataFrame()
         features = features.append(df, ignore_index=True)
