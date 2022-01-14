483edeca1cba045af47f131a293b89a2427e2715
LucaCogo <lucacogo997@gmail.com>
2022-01-14 17:00:12 +0100

    Fixed error cascade detector

--- src/detection/cascade/CascadeFaceDetector.py
+++ src/detection/cascade/CascadeFaceDetector.py
@@ -5,7 +5,7 @@ import os
 
 DEFAULT_MODEL_PATH = os.path.abspath('../detection/cascade/model/faceDetector_FDDB_LBP_10_0.01.xml')
 DEFAULT_MODEL_PATH = DEFAULT_MODEL_PATH.replace('\\', '/')
-DEFAULT_MODEL_PATH = 'C:/Users/Luca/Desktop/visual/src/detection/cascade/model/faceDetector_FDDB_LBP_10_0.01.xml'
+
 print(f'Local path:', DEFAULT_MODEL_PATH)
 
 class CascadeFaceDetector:
