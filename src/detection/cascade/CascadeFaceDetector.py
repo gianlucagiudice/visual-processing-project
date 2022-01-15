990066228bf9bf42fd7744fd80c04a0c218af9db
LucaCogo <lucacogo997@gmail.com>
2022-01-15 01:28:20 +0100

    Changed bbox convention for cascade detector

--- src/detection/cascade/CascadeFaceDetector.py
+++ src/detection/cascade/CascadeFaceDetector.py
@@ -3,7 +3,7 @@ from os.path import join
 import os
 
 
-DEFAULT_MODEL_PATH = os.path.abspath('../detection/cascade/model/faceDetector_FDDB_LBP_10_0.01.xml')
+DEFAULT_MODEL_PATH = os.path.abspath('../src/detection/cascade/model/faceDetector_FDDB_LBP_10_0.01.xml')
 DEFAULT_MODEL_PATH = DEFAULT_MODEL_PATH.replace('\\', '/')
 
 print(f'Local path:', DEFAULT_MODEL_PATH)
@@ -17,4 +17,9 @@ class CascadeFaceDetector:
 
     def detect_image(self, image):
         faces = self.cascade_model.detectMultiScale(image)
+
+        for bbox in faces:
+            bbox[2] = bbox[0] + bbox[2]
+            bbox[3] = bbox[1] + bbox[3]
+
         return faces
